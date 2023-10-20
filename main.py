f1c0840b41064d93106798a51e7a813bd9b9e7c5
VitaminB16 <artemiy.nosov@gmail.com>
2023-10-20 17:54:43 +0100

    Chore: pass arguments through main function

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def universe_game(nParticles, velocity, radius=1):
-    particlePos = np.random.rand(nParticles, 3)
+def universe_game(nParticles, velocity, radius=1, chance_for_global_radius=0.1, beta=1):
+    particlePos = np.random.rand(nParticles, 30)
     particlePos[:, 2] *= 360
 
     fig, ax = plt.subplots()
-    scatter = ax.scatter(particlePos[:, 0], particlePos[:, 1])
+    scatter = ax.scatter(particlePos[:, 0], particlePos[:, 1], alpha=0.5)
     lines = [ax.plot([0, 0], [0, 0], color="blue")[0] for _ in range(nParticles)]
     i = 0
     while True:
@@ -16,19 +16,20 @@ def universe_game(nParticles, velocity, radius=1):
         direction_angles = np.radians(particlePos[:, 2])
         cos_vals, sin_vals = np.cos(direction_angles), np.sin(direction_angles)
 
-        turns = where_to_turn(particlePos[:, :2], direction_angles, particlePos, radius)
+        turns = where_to_turn(
+            particlePos[:, :2],
+            direction_angles,
+            particlePos,
+            radius,
+            chance_for_global_radius,
+        )
+        turns = beta * turns
         particlePos[:, 2] = np.mod(particlePos[:, 2] + turns, 360)
 
         particlePos[:, 0] += velocity * cos_vals
         particlePos[:, 1] += velocity * sin_vals
 
         scatter.set_offsets(particlePos[:, :2])
-        for i, (coord, dir_angle, turn) in enumerate(
-            zip(particlePos[:, :2], direction_angles, turns)
-        ):
-            x0, y0 = coord
-            x1, y1 = x0 + 0.1 * np.cos(dir_angle), y0 + 0.1 * np.sin(dir_angle)
-            lines[i].set_data([x0, x1], [y0, y1])
 
         # Update the axis limits
         ax.set_xlim(particlePos[:, 0].min() - 0.1, particlePos[:, 0].max() + 0.1)
@@ -39,7 +40,11 @@ def universe_game(nParticles, velocity, radius=1):
     plt.show()
 
 
-def where_to_turn(coords, directions, particlePos, radius):
+def where_to_turn(
+    coords, directions, particlePos, radius, chance_for_global_radius=0.1, beta=1
+):
+    if np.random.rand() < chance_for_global_radius:
+        radius = 10000
     x0s, y0s = coords.T
     bearings = np.mod(
         np.degrees(
@@ -65,4 +70,4 @@ def where_to_turn(coords, directions, particlePos, radius):
 
 
 if __name__ == "__main__":
-    universe_game(200, 0.01)
+    universe_game(5000, 0.067, radius=5, chance_for_global_radius=0.1, beta=17)
