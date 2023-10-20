06a4eb9a07cfd8bfbca089b345a5a7b8adc7437e
VitaminB16 <artemiy.nosov@gmail.com>
2023-10-20 21:16:45 +0100

    Refactor: refactor of the UniverseGame class

--- universe_game/universe_game.py
+++ universe_game/universe_game.py
@@ -5,46 +5,33 @@ from time import time
 
 
 class UniverseGame:
-    def __init__(
-        self,
-        n_particles,
-        velocity,
-        radius=1,
-        chance_for_global_radius=0.1,
-        beta=1,
-        update_interval=1,
-        box_width=1,
-        clip_boundary=True,
-    ):
+    def __init__(self, n_particles, velocity, **kwargs):
         self.n_particles = n_particles
         self.velocity = velocity
-        self.radius = radius
-        self.chance_for_global_radius = chance_for_global_radius
-        self.beta = beta
-        self.update_interval = update_interval
-        self.box_width = box_width
-        self.clip_boundary = clip_boundary
-
-        self.particlePos = np.random.rand(n_particles, 3)
-        self.particlePos[:, 2] *= 360
-        self.particlePos[:, 0:2] *= box_width
-
+        self.radius = kwargs.get("radius", 1)
+        self.chance_for_global_radius = kwargs.get("chance_for_global_radius", 0.1)
+        self.beta = kwargs.get("beta", 1)
+        self.update_interval = kwargs.get("update_interval", 1)
+        self.box_width = kwargs.get("box_width", 1)
+        self.clip_boundary = kwargs.get("clip_boundary", True)
+        self.particlePos = self._initialize_particle_positions()
         self.time_prev = time()
 
+    def _initialize_particle_positions(self):
+        positions = np.random.rand(self.n_particles, 3)
+        positions[:, 2] *= 360
+        positions[:, 0:2] *= self.box_width
+        return positions
+
     def animate(self):
-        fig, ax = plt.subplots()
+        """Display the animation in a matplotlib animation."""
+        fig, ax = self._setup_plot()
         scatter = ax.scatter(self.particlePos[:, 0], self.particlePos[:, 1], alpha=0.5)
-        ax.set_xlim(0, self.box_width)
-        ax.set_ylim(0, self.box_width)
 
         def update(frame):
-            nonlocal scatter, ax
             self._update_particle_positions()
             scatter.set_offsets(self.particlePos[:, :2])
-
-            fps = 1 / (time() - self.time_prev)
-            self.time_prev = time()
-            ax.set_title(f"FPS: {fps:.2f}")
+            self._update_plot_title(ax)
 
             return (scatter,)
 
@@ -58,34 +45,60 @@ class UniverseGame:
         )
         plt.show()
 
+    def _setup_plot(self):
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, self.box_width)
+        ax.set_ylim(0, self.box_width)
+        return fig, ax
+
+    def _update_plot_title(self, ax):
+        fps = 1 / (time() - self.time_prev)
+        self.time_prev = time()
+        ax.set_title(f"FPS: {fps:.2f}")
+
     def _update_particle_positions(self):
+        self._move_particles_based_on_angle()
+        if self.clip_boundary:
+            self._handle_boundary()
+        turns = self._calculate_turns()
+        self._apply_turns(turns)
+
+    def _move_particles_based_on_angle(self):
         direction_angles = np.radians(self.particlePos[:, 2])
         cos_vals, sin_vals = np.cos(direction_angles), np.sin(direction_angles)
-        turns = self._where_to_turn()
-        self.particlePos[:, 2] = np.mod(self.particlePos[:, 2] + turns * self.beta, 360)
-
         self.particlePos[:, 0] += self.velocity * cos_vals
         self.particlePos[:, 1] += self.velocity * sin_vals
 
-        if self.clip_boundary:
-            self.particlePos = self._handle_boundary()
+    def _calculate_turns(self):
+        effective_radius = self._get_effective_radius()
+        dx, dy, distances = self._calculate_relative_distances()
+        relative_bearings = self._calculate_relative_bearings(dx, dy)
+        direction_turn = self._determine_turn_direction(
+            distances, relative_bearings, effective_radius
+        )
+        return direction_turn
 
-    def _where_to_turn(self):
-        n_particles = self.particlePos.shape[0]
-        global_radius_mask = np.random.rand(n_particles) < self.chance_for_global_radius
-        effective_radius = np.where(global_radius_mask, 1000, self.radius)
+    def _get_effective_radius(self, global_radius=1000):
+        global_radius_mask = (
+            np.random.rand(self.n_particles) < self.chance_for_global_radius
+        )
+        return np.where(global_radius_mask, global_radius, self.radius)
 
+    def _calculate_relative_distances(self):
         dx = self.particlePos[:, np.newaxis, 0] - self.particlePos[:, 0]
         dy = self.particlePos[:, np.newaxis, 1] - self.particlePos[:, 1]
         distances = np.sqrt(dx**2 + dy**2)
-        within_radius = distances < effective_radius[:, np.newaxis]
+        return dx, dy, distances
 
+    def _calculate_relative_bearings(self, dx, dy):
         bearings = np.arctan2(dy, dx)
         relative_bearings = np.degrees(bearings) - self.particlePos[:, 2, np.newaxis]
-        relative_bearings = (relative_bearings + 180) % 360 - 180
-        left_mask = within_radius & (relative_bearings <= 0)
-        right_mask = within_radius & (relative_bearings > 0)
+        return (relative_bearings + 180) % 360 - 180
 
+    def _determine_turn_direction(self, distances, relative_bearings, effective_radius):
+        left_mask, right_mask = self._get_turn_masks(
+            distances, relative_bearings, effective_radius
+        )
         leftCounter = left_mask.sum(axis=1)
         rightCounter = right_mask.sum(axis=1)
         direction_turn = np.sign(leftCounter - rightCounter)
@@ -95,15 +108,25 @@ class UniverseGame:
         )
         return direction_turn
 
-    def _handle_boundary(self):
-        left_bounce = self.particlePos[:, 0] < 0
-        right_bounce = self.particlePos[:, 0] > self.box_width
-        top_bounce = self.particlePos[:, 1] > self.box_width
-        bottom_bounce = self.particlePos[:, 1] < 0
+    def _get_turn_masks(self, distances, relative_bearings, effective_radius):
+        within_radius = distances < effective_radius[:, np.newaxis]
+        left_mask = within_radius & (relative_bearings <= 0)
+        right_mask = within_radius & (relative_bearings > 0)
+        return left_mask, right_mask
 
-        self.particlePos[left_bounce, 2] = 180 - self.particlePos[left_bounce, 2]
-        self.particlePos[right_bounce, 2] = 180 - self.particlePos[right_bounce, 2]
-        self.particlePos[top_bounce, 2] = 360 - self.particlePos[top_bounce, 2]
-        self.particlePos[bottom_bounce, 2] = 360 - self.particlePos[bottom_bounce, 2]
+    def _apply_turns(self, turns):
+        self.particlePos[:, 2] = np.mod(self.particlePos[:, 2] + turns * self.beta, 360)
+
+    def _handle_boundary(self):
+        self._reflect_off_boundaries()
         self.particlePos[:, 2] = np.mod(self.particlePos[:, 2], 360)
-        return self.particlePos
+
+    def _reflect_off_boundaries(self):
+        conditions = [
+            (self.particlePos[:, 0] < 0, 180),
+            (self.particlePos[:, 0] > self.box_width, 180),
+            (self.particlePos[:, 1] > self.box_width, 360),
+            (self.particlePos[:, 1] < 0, 360),
+        ]
+        for condition, angle in conditions:
+            self.particlePos[condition, 2] = angle - self.particlePos[condition, 2]
