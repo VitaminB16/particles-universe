39227a32ab141d71cfd6522b0cadec6daf811954
VitaminB16 <artemiy.nosov@gmail.com>
2023-10-22 20:40:21 +0100

    Improved: pygame camera behaviour

--- universe_game/pygame_utils.py
+++ universe_game/pygame_utils.py
@@ -1,10 +1,17 @@
+import numpy as np
 
 def compute_dynamic_scale_and_offset(particles, target_dim, padding=0.0):
     """Compute scale and offset to fit the particles within the target dimensions."""
-    min_x, min_y = particles[:, 0].min() - padding, particles[:, 1].min() - padding
-    max_x, max_y = particles[:, 0].max() + padding, particles[:, 1].max() + padding
-    min_min = min(min_x, min_y)
-    max_max = max(max_x, max_y)
+    min_x = np.percentile(particles[:, 0], 3)
+    min_y = np.percentile(particles[:, 1], 3)
+    max_x = np.percentile(particles[:, 0], 97)
+    max_y = np.percentile(particles[:, 1], 97)
+
+    padding = padding * (max(max_x - min_x, max_y - min_y))
+    min_x -= padding
+    min_y -= padding
+    max_x += padding
+    max_y += padding
 
     scale_x = target_dim[0] / (max_x - min_x)
     scale_y = target_dim[1] / (max_y - min_y)
