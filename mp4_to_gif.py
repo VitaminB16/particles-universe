24c07e3407bd6a685ad949db0689d8d7ce9b38a3
VitaminB16 <artemiy.nosov@gmail.com>
2023-10-25 16:06:25 +0100

    Chore: complete readme, more conds

--- mp4_to_gif.py
+++ mp4_to_gif.py
@@ -1,10 +1,11 @@
 from moviepy.editor import VideoFileClip
 import moviepy.video.fx.all as vfx
 
+
 def mp4_to_gif(input_file, output_file, skip_frames=1, resize=1):
     """
     Convert MP4 video to GIF.
-    
+
     Parameters:
     - input_file: str: path to the input .mp4 file
     - output_file: str: path to the output .gif file
@@ -20,11 +21,13 @@ def mp4_to_gif(input_file, output_file, skip_frames=1, resize=1):
         # clip.duration = duration // skip_frames
         # Resize the clip based on the downscaling factor
         if resize != 1:
-            clip = clip.resize(1/resize)
+            clip = clip.resize(1 / resize)
 
         clip.write_gif(output_file, fps=60 // skip_frames)
 
 
+if __name__ == "__main__":
+    import sys
 
-# Usage:
-mp4_to_gif("swarm.mp4", "swarm.gif", skip_frames=6, resize=2)
+    args = sys.argv[1:][0]
+    mp4_to_gif(args, args.replace(".mp4", ".gif"), skip_frames=3, resize=2)
